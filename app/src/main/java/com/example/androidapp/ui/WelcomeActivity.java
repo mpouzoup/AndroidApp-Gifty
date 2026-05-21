9b1021fc216902548d01e01d66dbd4710127c33b
Bouzouki Polyxeni <mpouzoup@csd.auth.gr>
2026-05-21 21:55:01 +0300

    welcome activity

--- app/src/main/java/com/example/androidapp/ui/WelcomeActivity.java
+++ app/src/main/java/com/example/androidapp/ui/WelcomeActivity.java
@@ -2,30 +2,27 @@ package com.example.androidapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler; // Απαραίτητο για το χρονόμετρο
+import android.os.Handler;
+import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidapp.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-    // Ορίζουμε τον χρόνο που θα κρατήσει η οθόνη (3000 milliseconds = 3 δευτερόλεπτα)
-    private static final int SPLASH_TIME_OUT = 3000;
+    private static final int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Ο Handler αναλαμβάνει να περιμένει 3 δευτερόλεπτα και μετά να εκτελέσει το Intent
-        new Handler().postDelayed(new Runnable() {
+        setContentView(R.layout.activity_welcome);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
                 startActivity(intent);
 
-                // Κλείνουμε τη WelcomeActivity για να μην μπορεί ο χρήστης να γυρίσει εδώ με το Back
                 finish();
-
             }
         }, SPLASH_TIME_OUT);
     }
